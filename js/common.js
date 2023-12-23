befe94d4d3504ee00fcce791beec8342e2dfb1d5
Suhyun Kim <kim00431@algonquinlive.com>
2023-12-22 19:37:51 -0500

    updates

--- js/common.js
+++ js/common.js
@@ -8,3 +8,15 @@ $(slideWrap).slick({
     // adaptiveHeight: true
   });
 
+
+const modal_cmt = () =>{
+  const modal_container = document.querySelector('.modal_cmt_container')
+  const open_cmt = document.querySelector('.open_cmt');
+  
+  //open_modal
+
+
+
+
+  //close_modal
+}
